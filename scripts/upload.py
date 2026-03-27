467aa68d0c49d10ba450ce3bdcd3e8038a1df0fd
Abdulla A. B. <babulwriting@gmail.com>
2026-03-27 08:16:09 +0600

    Update upload.py

--- scripts/upload.py
+++ scripts/upload.py
@@ -1,1 +1,22 @@
+import requests
+import json
+import os
+WC_URL = "https://miralooks.com/wp-json/wc/v3/helloproducts"
+CK = "ck_8971f5fb93a23b4d8f2e079f0a7a002c720c7c97"
+CS = "cs_6de72383d046244c94c3075e736aac6f3353fd48"
+
+folder = "helloproducts"
+
+for file in os.listdir(folder):
+  if file.endwith(".json"):
+     with open(f"{folder}/{file}") as f:
+       data = json.load(f)
+
+     r = requests.post(
+       WC_URL,
+       auth=(CK, CS),
+       jason=data
+     )
+
+     print(file, r.status_code)
 
